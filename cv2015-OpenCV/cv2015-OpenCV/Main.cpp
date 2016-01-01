7928c940d00d0418d45197beeb2e2dc5d6866c18
Daniel <daniel.silva@ua.pt>
2016-01-01 23:14:47 +0000

    Added Robert's Edge Detector.

--- cv2015-OpenCV/cv2015-OpenCV/Main.cpp
+++ cv2015-OpenCV/cv2015-OpenCV/Main.cpp
@@ -19,21 +19,28 @@ using namespace cv;
 using namespace std;
 
 /// Global variables
-Mat imgOrig;
-Mat canny_img, canny_gray;
-Mat dst, detected_edges;
-Mat grad, sobel_img, sobel_gray;
+Mat imgOrig; // Original image
+Mat canny_img, canny_gray; // Canny
+Mat canny_dst, detected_edges; // Canny
+Mat sobel_grad, sobel_img, sobel_gray; // Sobel
+Mat robert_grad, robert_img, robert_gray; // Robert
 
 int edgeThresh = 1;
 int lowThreshold;
 int const max_lowThreshold = 255;
 int ratio = 3;
 int kernel_size = 3;
-string windowName[] = { "Original Image", "Canny Edge Detector", "Sobel Edge Detector" };
+string windowName[] = { "Original Image", "Canny Edge Detector", "Sobel Edge Detector", "Robert's Edge Detector" };
 
 /// Functions
 void CannyThreshold(int, void*);
 void SobelDerivatives();
+void RobertsDetector();
+
+// Auxiliar functions
+int xGradient(Mat image, int x, int y);
+int yGradient(Mat image, int x, int y);
+void conv2(Mat src, int kernel_size);
 void printImageFeatures(const Mat &imagem);
 
 
@@ -72,7 +79,7 @@ int main( void ) /// int argc, char** argv
 
 	// --------------- Canny Edge Detector ----------------
 	// Create a matrix of the same type and size as src (for dst)
-	dst.create(imgOrig.size(), imgOrig.type());
+	canny_dst.create(imgOrig.size(), imgOrig.type());
 	canny_img = imgOrig.clone();
 	// Convert the image to grayscale
 	cvtColor(canny_img, canny_gray, COLOR_BGR2GRAY);
@@ -83,19 +90,29 @@ int main( void ) /// int argc, char** argv
 	CannyThreshold(0, 0);
 
 	cout << endl << "Canny Image:";
-	printImageFeatures(dst); // Show information about image
+	printImageFeatures(canny_dst); // Show information about image
 
 
 	// --------------- Sobel Edge Detector ----------------
 	sobel_img = imgOrig.clone();
 	// Convert the image to grayscale
-	cvtColor(imgOrig, sobel_gray, COLOR_BGR2GRAY);
+	cvtColor(sobel_img, sobel_gray, COLOR_BGR2GRAY);
 	// Call function
 	SobelDerivatives();
 
 	cout << endl << "Sobel Image:";
-	printImageFeatures(grad); // Show information about image
+	printImageFeatures(sobel_grad); // Show information about image
 
+
+	// ------------- Robert's Edge Detector ---------------
+	robert_img = imgOrig.clone();
+	// Convert the image to grayscale
+	cvtColor(robert_img, robert_gray, COLOR_BGR2GRAY);
+	// Call function
+	RobertsDetector();
+
+	cout << endl << "Robert's Image:";
+	printImageFeatures(robert_gray); // Show information about image
 
 
 	/// FINAL ---------------------------------------------
@@ -120,10 +137,10 @@ void CannyThreshold(int, void*) {
 	Canny(detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size);
 
 	/// Using Canny's output as a mask, we display our result
-	dst = Scalar::all(0);
+	canny_dst = Scalar::all(0);
 
-	canny_img.copyTo(dst, detected_edges);
-	imshow(windowName[1], dst);
+	canny_img.copyTo(canny_dst, detected_edges);
+	imshow(windowName[1], canny_dst);
 }
 
 
@@ -137,9 +154,7 @@ void SobelDerivatives() {
 	int delta = 0;
 	int ddepth = CV_16S;
 
-	GaussianBlur(sobel_img, sobel_img, Size(3, 3), 0, 0, BORDER_DEFAULT);
-
-	namedWindow(windowName[2], WINDOW_AUTOSIZE); // Window
+	GaussianBlur(sobel_gray, sobel_gray, Size(3, 3), 0, 0, BORDER_DEFAULT);
 	
 	// Generate grad_x and grad_y
 	Mat grad_x, grad_y;
@@ -154,8 +169,62 @@ void SobelDerivatives() {
 	convertScaleAbs(grad_y, abs_grad_y);
 	
 	/// Total Gradient (approximate)
-	addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad);
-	imshow(windowName[2], grad);
+	addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, sobel_grad);
+
+	namedWindow(windowName[2], WINDOW_AUTOSIZE); // Window
+	imshow(windowName[2], sobel_grad);
+
+	/// OTHER VERSION
+/*
+	Mat sobelx;
+	Sobel(sobel_gray, sobelx, CV_32F, 1, 0);
+
+	double minVal, maxVal;
+	minMaxLoc(sobelx, &minVal, &maxVal); //find minimum and maximum intensities
+	cout << "minVal : " << minVal << endl << "maxVal : " << maxVal << endl;
+
+	Mat draw;
+	sobelx.convertTo(draw, CV_8U, 255.0 / (maxVal - minVal), -minVal * 255.0 / (maxVal - minVal));
+
+	namedWindow("image", CV_WINDOW_AUTOSIZE);
+	imshow("image", draw);
+*/
+}
+
+
+/**
+* @function RobertsDetector
+* @brief Trackbar callback - Applies a convolution, then its like Sobel, applying gradiant.
+*/
+void RobertsDetector() {
+	int scale = 1;
+	int delta = 0;
+	int ddepth = CV_16S;
+
+	GaussianBlur(robert_gray, robert_gray, Size(3, 3), 0, 0, BORDER_DEFAULT);
+	
+	int mx[4] = { 1, 0, 0, -1 };
+	Matx22f matx = Matx22f(1, 0, 0, -1);
+	InputArray kernel = _InputArray(matx);
+	
+
+	// Generate grad_x and grad_y
+	Mat grad_x, grad_y;
+	Mat abs_grad_x, abs_grad_y;
+
+	/// Gradient X
+	filter2D(robert_gray, grad_x, -1, kernel, Point(-1, -1), 0, BORDER_DEFAULT);
+	convertScaleAbs(grad_x, abs_grad_x);
+
+	/// Gradient Y
+	filter2D(robert_gray, grad_y, -1, kernel, Point(-1, -1), 0, BORDER_DEFAULT);
+	convertScaleAbs(grad_y, abs_grad_y);
+
+	/// Total Gradient (approximate)
+	addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, robert_grad);
+
+	namedWindow(windowName[3], WINDOW_AUTOSIZE); // Window
+	imshow(windowName[3], robert_grad);
 }
 
 
