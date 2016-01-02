d3c87c709ae2b1754eb532e7ec142ce3e07ef2c4
Daniel <daniel.silva@ua.pt>
2016-01-02 03:28:35 +0000

    Prewitt's Edge Detector.

--- cv2015-OpenCV/cv2015-OpenCV/Main.cpp
+++ cv2015-OpenCV/cv2015-OpenCV/Main.cpp
@@ -24,23 +24,25 @@ Mat canny_img, canny_gray; // Canny
 Mat canny_dst, detected_edges; // Canny
 Mat sobel_grad, sobel_img, sobel_gray; // Sobel
 Mat robert_grad, robert_img, robert_gray; // Robert
+Mat prewitt_grad, prewitt_img, prewitt_gray; // Prewitt
+Mat freichen_grad, freichen_img, freichen_gray; // Frei Chen
 
 int edgeThresh = 1;
 int lowThreshold;
 int const max_lowThreshold = 255;
 int ratio = 3;
 int kernel_size = 3;
-string windowName[] = { "Original Image", "Canny Edge Detector", "Sobel Edge Detector", "Robert's Edge Detector" };
+string windowName[] = { "Original Image", "Canny Edge Detector", "Sobel Edge Detector", "Robert's Edge Detector",
+						"Prewitt Edge Detector", "Frei Chen Detector" };
 
 /// Functions
 void CannyThreshold(int, void*);
 void SobelDerivatives();
 void RobertsDetector();
+void PrewittDetector();
+void FreiChenDetector();
 
 // Auxiliar functions
-int xGradient(Mat image, int x, int y);
-int yGradient(Mat image, int x, int y);
-void conv2(Mat src, int kernel_size);
 void printImageFeatures(const Mat &imagem);
 
 
@@ -53,13 +55,13 @@ int main( void ) /// int argc, char** argv
 	cout << "\tSobel Edge Detector" << endl;
 	cout << "\tRobert's Edge Detector" << endl;
 	cout << "\tPrewitt Edge Detector" << endl;
-	cout << "\tFrie Chen Edge Detector" << endl;
+	cout << "\tFrei Chen Edge Detector" << endl;
 	cout << "\tMarr-Hildreth Edge Detector" << endl;
 	
 	// Read image name
 	string imgName;
 	cout << endl << "Choose an image name:(see img folder) " << endl;
-	cout << "[ lena.jpg, deti.jpg, sta2.bmp, wdg2.bmp, tools_2.png, mon1.bmp, ape1.bmp ]" << endl;
+	cout << "[ lena.jpg, deti.jpg, sta2.bmp, wdg2.bmp, tools_2.png, mon1.bmp, ape1.bmp Bikesgray.jpg ]" << endl;
 	cout << endl << "Photo name: ";
 	cin >> imgName;
 
@@ -112,7 +114,20 @@ int main( void ) /// int argc, char** argv
 	RobertsDetector();
 
 	cout << endl << "Robert's Image:";
-	printImageFeatures(robert_gray); // Show information about image
+	printImageFeatures(robert_grad); // Show information about image
+
+
+	// -------------- Prewitt Edge Detector ----------------
+	prewitt_img = imgOrig.clone();
+	// Convert the image to grayscale
+	cvtColor(prewitt_img, prewitt_gray, COLOR_BGR2GRAY);
+	// Call function
+	PrewittDetector();
+
+	cout << endl << "Prewitt Image:";
+	printImageFeatures(prewitt_grad); // Show information about image
+
+
 
 
 	/// FINAL ---------------------------------------------
@@ -197,15 +212,13 @@ void SobelDerivatives() {
 * @brief Trackbar callback - Applies a convolution, then its like Sobel, applying gradiant.
 */
 void RobertsDetector() {
-	int scale = 1;
-	int delta = 0;
-	int ddepth = CV_16S;
 
 	GaussianBlur(robert_gray, robert_gray, Size(3, 3), 0, 0, BORDER_DEFAULT);
 	
-	int mx[4] = { 1, 0, 0, -1 };
 	Matx22f matx = Matx22f(1, 0, 0, -1);
-	InputArray kernel = _InputArray(matx);
+	Matx22f maty = Matx22f(0, 1, -1, 0);
+	InputArray kernelx = _InputArray(matx);
+	InputArray kernely = _InputArray(maty);
 	
 
 	// Generate grad_x and grad_y
@@ -213,11 +226,11 @@ void RobertsDetector() {
 	Mat abs_grad_x, abs_grad_y;
 
 	/// Gradient X
-	filter2D(robert_gray, grad_x, -1, kernel, Point(-1, -1), 0, BORDER_DEFAULT);
+	filter2D(robert_gray, grad_x, -1, kernelx, Point(-1, -1), 0, BORDER_DEFAULT);
 	convertScaleAbs(grad_x, abs_grad_x);
 
 	/// Gradient Y
-	filter2D(robert_gray, grad_y, -1, kernel, Point(-1, -1), 0, BORDER_DEFAULT);
+	filter2D(robert_gray, grad_y, -1, kernely, Point(-1, -1), 0, BORDER_DEFAULT);
 	convertScaleAbs(grad_y, abs_grad_y);
 
 	/// Total Gradient (approximate)
@@ -226,6 +239,43 @@ void RobertsDetector() {
 	namedWindow(windowName[3], WINDOW_AUTOSIZE); // Window
 	imshow(windowName[3], robert_grad);
 }
+
+
+/**
+* @function PrewittDetector
+* @brief Trackbar callback - Applies a different convolution of robert's, then its like Sobel, applying gradiant.
+*/
+void PrewittDetector() {
+
+	GaussianBlur(prewitt_gray, prewitt_gray, Size(3, 3), 0, 0, BORDER_DEFAULT);
+
+	Matx33f matx = Matx33f(-1, 0, 1, -1, 0, 1, -1, 0, 1);
+	Matx33f maty = Matx33f(-1, -1, -1, 0, 0, 0, 1, 1, 1);
+	InputArray kernelx = _InputArray(matx);
+	InputArray kernely = _InputArray(maty);
+
+
+	// Generate grad_x and grad_y
+	Mat grad_x, grad_y;
+	Mat abs_grad_x, abs_grad_y;
+
+	/// Gradient X
+	filter2D(prewitt_gray, grad_x, -1, kernelx, Point(-1, -1), 0, BORDER_DEFAULT);
+	convertScaleAbs(grad_x, abs_grad_x);
+
+	/// Gradient Y
+	filter2D(prewitt_gray, grad_y, -1, kernely, Point(-1, -1), 0, BORDER_DEFAULT);
+	convertScaleAbs(grad_y, abs_grad_y);
+
+	/// Total Gradient (approximate)
+	addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, prewitt_grad);
+
+	namedWindow(windowName[4], WINDOW_AUTOSIZE); // Window
+	imshow(windowName[4], prewitt_grad);
+}
+
+
+
 
 
 // FUNCOES AUXILIARES
